506c5701f4c263809d5dd43f83536a178b7531ad
inam343 <inamtariqsatti7@gmail.com>
2026-07-28 03:43:13 -0700

    Update nav.jsx

--- src/componant/nav.jsx
+++ src/componant/nav.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className="py-3  ">
             <div className="flex items-center justify-center gap-5 sm:gap-8 md:gap-10  bg-white  sm:bg-transparent md:bg-transparent  h-[40px] rounded-lg sm:px-15 md:px-15 lg:px-15  ">
-                <Link href="/" className=" text-[15px] sm:text-[16px] md:text-[18px]   sm:text-gray-800 md:text-gray-800 lg:text-gray-800 font-[600] hover:text-green-600">Home</Link>
+                <Link href="/" className=" text-[15px] sm:text-[16px] md:text-[18px] text-gray-800  sm:text-gray-800 md:text-gray-800 lg:text-gray-800 font-[600] hover:text-green-600">Home</Link>
                 <Link href="/fruits" className="text-[15px] sm:text-[16px] md:text-[18px] text-gray-800 font-[600] hover:text-green-600">Fruits</Link>
                 <Link href="/counter" className="text-[15px] sm:text-[16px] md:text-[18px] text-gray-800 font-[600] hover:text-green-600">Meats</Link>
                 <Link href="/breakfast" className="text-[15px] sm:text-[16px] md:text-[18px] text-gray-800 font-[600] hover:text-green-600">Dairy</Link>
