4c3938e371014dea0d077634187cec140fbf4e1f
inam343 <inamtariqsatti7@gmail.com>
2026-07-28 03:29:53 -0700

    Update nav.jsx

--- src/componant/nav.jsx
+++ src/componant/nav.jsx
@@ -6,7 +6,7 @@ import { FaAngleDown } from "react-icons/fa";
 const Navbar = () => {
     return (
         <nav className="py-3 ">
-            <div className="flex items-center justify-center gap-10 bg-[#202124]  sm:bg-transparent md:bg-transparent  h-[40px] rounded-lg sm:px-15 md:px-15 lg:px-15  ">
+            <div className="flex items-center justify-center gap-5 sm:gap-8 md:gap-10 bg-[#202124]  sm:bg-transparent md:bg-transparent  h-[40px] rounded-lg sm:px-15 md:px-15 lg:px-15  ">
                 <Link href="/" className=" text-[15px] sm:text-[16px] md:text-[18px]   sm:text-gray-800 md:text-gray-800 lg:text-gray-800 font-[600] hover:text-green-600">Home</Link>
                 <Link href="/fruits" className="text-[15px] sm:text-[16px] md:text-[18px] text-gray-800 font-[600] hover:text-green-600">Fruits</Link>
                 <Link href="/counter" className="text-[15px] sm:text-[16px] md:text-[18px] text-gray-800 font-[600] hover:text-green-600">Meats</Link>
