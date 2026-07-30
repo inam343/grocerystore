dd451bddf88ad379a1684d194defdba097229d78
inam343 <inamtariqsatti7@gmail.com>
2026-07-30 04:08:01 -0700

    Update page.jsx

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -8,7 +8,7 @@ const AddToCart = () => {
   const { cartItems, cartCount } = useCart();
 
   return (
-    <div className="flex justify-center items-center bg-[#FAFAFA] min-h-screen">
+    <div className="flex justify-center top-3 items-center bg-[#FAFAFA] min-h-screen">
       <div className="bg-white w-[500px] p-4 m-6 rounded-md">
         <div className="gap-2">
           <h2 className="font-[600] dark:text-black text-[15px]">Your Cart</h2>
