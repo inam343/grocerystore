9d392b4971ef4d2a4815eb7cf418082aaaaabc23
inam343 <inamtariqsatti7@gmail.com>
2026-07-30 04:07:18 -0700

    Update page.jsx

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -11,7 +11,7 @@ const AddToCart = () => {
     <div className="flex justify-center items-center bg-[#FAFAFA] min-h-screen">
       <div className="bg-white w-[500px] p-4 m-6 rounded-md">
         <div className="gap-2">
-          <h2 className="font-[600] text-[15px]">Your Cart</h2>
+          <h2 className="font-[600] dark:text-black text-[15px]">Your Cart</h2>
           <p className="font-[300]">
             {cartCount === 0
               ? "Your cart is empty"
@@ -21,7 +21,7 @@ const AddToCart = () => {
         <hr className="border-zinc-800 my-3" />
 
         {cartItems.length === 0 ? (
-          <p className="text-center text-gray-400 py-10">
+          <p className="text-center dark:text-black text-gray-400 py-10">
             No items added yet. Start shopping!
           </p>
         ) : (
