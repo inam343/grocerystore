07a9fc4af82cc06c151640021645f5c6d54db462
inam343 <inamtariqsatti7@gmail.com>
2026-07-30 04:04:31 -0700

    Update search.jsx

--- src/componant/search.jsx
+++ src/componant/search.jsx
@@ -3,7 +3,7 @@ import { IoIosSearch } from "react-icons/io";
 const Search=()=>
 {
     return(
-<div className='search bg-[#E6E6E6] h-[30px] sm:w-[500px] w-[100px] rounded-md px-4 relative border border-[rgba(0,0,0,0.1)] hover:border-[rgba(0,0,0,0.5)]'>
+<div className='search bg-[#E6E6E6] h-[30px] sm:w-[500px] dark:bg-transparent w-[100px] rounded-md px-4 relative border border-[rgba(0,0,0,0.1)] hover:border-[rgba(0,0,0,0.5)]'>
     <input className="text-black" type="text"  className="h-full w-full outline-none border-0 " placeholder="search " />
   <button><IoIosSearch size={20} className="absolute top-1 right-1  cursor-pointer" /></button>
 </div>
