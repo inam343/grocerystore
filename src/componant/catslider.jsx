5318af9453969c93b48f2da1d01d3685577b8694
inam343 <inamtariqsatti7@gmail.com>
2026-09-14 16:03:02 +0500

    Update catslider.jsx

--- src/componant/catslider.jsx
+++ src/componant/catslider.jsx
@@ -13,7 +13,7 @@ const Catslider = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("https://server-production-5112.up.railway.app/api/categories")
+    fetch("https://server-production-8923.up.railway.app/api/categories")
       .then((res) => res.json())
       .then((data) => {
         setCategories(data);
