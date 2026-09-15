2d972783c570476ab823efecb86a8dfbae9db5d3
inam343 <inamtariqsatti7@gmail.com>
2026-09-15 12:43:24 +0500

    Update cartcard.jsx

--- src/componant/cartcard.jsx
+++ src/componant/cartcard.jsx
@@ -26,7 +26,7 @@ const Cartcard = ({ product }) => {
         {/* Discount badge */}
         {discount && (
           <span className="absolute top-2 left-2 z-10 bg-red-500 text-white text-[10px] font-bold px-2 py-0.5 rounded-full">
-            -{discount}%
+            {discount}%
           </span>
         )}
         {/* Remove button */}
